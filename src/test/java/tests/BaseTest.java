d33fe782fb6e6c67d4212b1555dc3cccb86e28e5
Pavel Dashevski <paveldashevski@gmail.com>
2021-02-17 20:10:49 +0300

    baseTest upd

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import pages.CartPage;
+import pages.CheckoutPage;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.util.concurrent.TimeUnit;
+
+@Listeners(TestListener.class)
+public class BaseTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
+    CheckoutPage checkoutPage;
+
+    @BeforeMethod
+    public void setup(ITestResult testResult) {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutPage = new CheckoutPage(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
