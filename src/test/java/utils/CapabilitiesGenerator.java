5e133e37245ba914f17f3dd78f2b929536fe9b64
Pavel Dashevski <paveldashevski@gmail.com>
2021-03-18 20:56:17 +0300

    CapabilitiesGenerator add

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -6,11 +6,11 @@ public class CapabilitiesGenerator {
 
     public static ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
-        String driverPath = "src/test/resources";
+        String driverPath = "src/test/resources/webdrivers/";
         String os = System.getProperty("os.name").toLowerCase();
         System.out.println(("Operational system: " + os + "; Driver path: " + driverPath));
         if (os.contains("win")) {
-            System.setProperty("webdriver.chrome.driver", driverPath + "/webdrivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
         } else if (os.contains("mac")) {
             System.setProperty("webdriver.chrome.driver", driverPath + "/chromedriver");
         } else {
